1c0cc6c5aca11c8fd06cb0670b024a09c801932e
Yasser Greyeb <ygreyeb@gmail.com>
2023-10-04 18:49:12 -0500

    Generate Baloto number

--- src/compute.ts
+++ src/compute.ts
@@ -1,35 +1,17 @@
-const array = new Int8Array(3);
-self.crypto.getRandomValues(array);
+const baloto = {
+  balls: Array.from({ length: 43 }, (_, i) => i + 1),
+  sample: 5,
+  combinations: 962598,
+  numbers: 16 * 962598,
+};
 
-const combinationsCache = new Map<number, number>();
-
-export function combinations(n: number, r: number): number {
-  if (n < r) {
-    return 0;
+function random24bits(): number {
+  const array = new Uint8Array(3);
+  self.crypto.getRandomValues(array);
+  let result = 0;
+  for (let i = 0; i < array.length; i++) {
+    result |= array[i] << (i * 8);
   }
-
-  const key = computeKey(n, r);
-  const retrieved = combinationsCache.get(key);
-  if (retrieved !== undefined) {
-    return retrieved;
-  }
-
-  let result = n;
-  for (let i = 1; i < r; i++) {
-    result *= n - i;
-  }
-  result /= factorial(r);
-
-  combinationsCache.set(key, result);
-  return result;
-}
-
-const prime = 31;
-
-function computeKey(n: number, r: number): number {
-  let result = 7;
-  result = prime * result + n;
-  result = prime * result + r;
   return result;
 }
 
@@ -47,4 +29,74 @@ export function factorial(n: number): number {
     factorials.push(last);
   }
   return last;
+}
+
+const prime = 31;
+
+function computeKey(n: number, r: number): number {
+  let result = 7;
+  result = prime * result + n;
+  result = prime * result + r;
+  return result;
+}
+
+const combinationsMemo = new Map<number, number>();
+
+export function combinations(elements: number, sample: number): number {
+  if (elements < sample) {
+    throw new Error("sample is greater than the number of elements", {
+      cause: { code: "InvalidArgument", values: [elements, sample] }
+    });
+  }
+
+  const key = computeKey(elements, sample);
+  const retrieved = combinationsMemo.get(key);
+  if (retrieved !== undefined) {
+    return retrieved;
+  }
+
+  let result = elements;
+  for (let i = 1; i < sample; i++) {
+    result *= elements - i;
+  }
+  result /= factorial(sample);
+
+  combinationsMemo.set(key, result);
+  return result;
+}
+
+export function computeCombination<T>(elements: T[], sample: number, ordinal: number): T[] {
+  const out = new Array<T>(sample);
+
+  let s = sample;
+  let o = ordinal;
+  for (let n = elements.length - 1; n >= 0; n--) {
+    const y = (n >= s) ? combinations(n, s) : 0;
+    if (o >= y) {
+      o -= y;
+      s--;
+      out[s] = elements[n];
+      if (s == 0) {
+        break;
+      }
+    }
+  }
+  return out;
+}
+
+export type Ticket = {
+  special: number;
+  regular: number[];
+}
+
+export function generate(): Ticket {
+  let rand: number;
+  do {
+    rand = random24bits();
+  } while (rand >= baloto.numbers);
+
+  const special = Math.floor(rand / baloto.combinations) + 1;
+  const ordinal = rand % baloto.combinations;
+  const regular = computeCombination(baloto.balls, baloto.sample, ordinal);
+  return { special, regular };
 }
--- test/compute.test.ts
+++ test/compute.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { combinations, factorial } from '../src/compute';
+import { combinations, computeCombination, factorial, generate } from '../src/compute';
 
 test('factorial(5) to equal 120', () => {
   expect(factorial(5)).toBe(120);
@@ -13,8 +13,8 @@ test('combinations(4,3) to equal 4', () => {
   expect(combinations(4, 3)).toBe(4);
 });
 
-test('combinations(3,4) to equal 0', () => {
-  expect(combinations(3, 4)).toBe(0);
+test('combinations(3,4) to throw', () => {
+  expect(() => combinations(3, 4)).toThrowError();
 });
 
 test('combinations(5,5) to equal 1', () => {
@@ -28,3 +28,44 @@ test('combinations(8,3) to equal 56', () => {
 test('combinations(43,5) to equal 962598', () => {
   expect(combinations(43, 5)).toBe(962598);
 });
+
+test('computeCombination(1-8,3,0) to be [1,2,3]', () => {
+  expect(computeCombination([1, 2, 3, 4, 5, 6, 7, 8], 3, 0)).toStrictEqual([1, 2, 3]);
+});
+
+test('computeCombination(1-8,3,7) to be [1,4,5]', () => {
+  expect(computeCombination([1, 2, 3, 4, 5, 6, 7, 8], 3, 7)).toStrictEqual([1, 4, 5]);
+});
+
+test('computeCombination(1-8,3,11) to be [1,3,6]', () => {
+  expect(computeCombination([1, 2, 3, 4, 5, 6, 7, 8], 3, 11)).toStrictEqual([1, 3, 6]);
+});
+
+test('generate() produces valid Baloto numbers', () => {
+  const ticket = generate();
+
+  // checks special ball is an integer between 1 and 16 inclusive
+  expect(ticket.special).toSatisfy(betweenInclusive(1, 16));
+  expect(ticket.special).toSatisfy(isInteger);
+
+  expect(ticket.regular.length).toBe(5);
+  expect(ticket.regular).toSatisfy(allUnique);
+  ticket.regular.forEach(r => {
+    expect(r).toSatisfy(betweenInclusive(1, 43));
+    expect(r).toSatisfy(isInteger);
+  });
+
+}, { repeats: 1000 });
+
+function betweenInclusive(lower: number, upper: number): (value: number) => boolean {
+  return value => value >= lower && value <= upper;
+}
+
+function isInteger(value: number): boolean {
+  return value === Math.round(value);
+}
+
+function allUnique(numbers: number[]): boolean {
+  const unique = numbers.filter((value, idx) => numbers.indexOf(value) === idx);
+  return unique.length === numbers.length;
+}
